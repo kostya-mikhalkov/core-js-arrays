8724f74510ea78ac8adf4eb610ba09888cc94d00
Kostya <kostya.mikhalkov@bk.ru>
2024-01-15 20:23:57 +0300

    feat: complete sumArrays and findElement

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,12 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const arr1Length = arr1.length;
+  const arr2Length = arr2.length;
+  const n1 = arr1Length >= arr2Length ? arr1 : arr2;
+  const n2 = arr1Length < arr2Length ? arr1 : arr2;
+  return Array.from(n1, (item, ind) => item + (n2[ind] || 0));
 }
 
 /**
@@ -53,8 +57,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
