d4f347ce48d26c26f03c621b662c29010034a13b
Kostya <kostya.mikhalkov@bk.ru>
2024-01-17 20:08:36 +0300

    feat: complete getHead

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -193,8 +193,9 @@ function insertItem(arr, item, index) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  const arrSlice = arr.slice(0, n);
+  return arrSlice;
 }
 
 /**
