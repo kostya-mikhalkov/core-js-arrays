5e08d3cff13b05ccd1c8d65b26801d25395aab6d
Kostya <kostya.mikhalkov@bk.ru>
2024-01-17 19:33:38 +0300

    feat: complete isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -146,8 +146,9 @@ function getAverage(arr, acc = 0) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const firstElemLength = arr[0].length;
+  return arr.every((item) => item.length === firstElemLength);
 }
 
 /**
