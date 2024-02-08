c915fff054660209b9f88ea6ecc66c77bf3b4c3f
Kostya <kostya.mikhalkov@bk.ru>
2024-02-08 20:58:13 +0300

    feat: complete getHexRGBValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -472,8 +472,13 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  const arrMap = arr.map((item) => {
+    const color = item.toString(16).padStart(6, 0).toUpperCase();
+    const rgb = `#${color}`;
+    return rgb;
+  });
+  return arrMap;
 }
 
 /**
