d5328d53ebf1b8debd5ec79f0ee8440895d61f8b
Kostya <kostya.mikhalkov@bk.ru>
2024-02-11 12:14:16 +0300

    feat: complete shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -572,8 +572,16 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const res = [];
+  if (n > 0) {
+    res.push(arr.slice(arr.length - n, arr.length));
+    res.push(arr.slice(0, arr.length - n));
+  } else {
+    res.push(arr.slice(Math.abs(n), arr.length));
+    res.push(arr.slice(0, Math.abs(n)));
+  }
+  return res.flat();
 }
 
 /**
